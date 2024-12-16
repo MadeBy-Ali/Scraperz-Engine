82b4256b37e30ea4703544b4e11e24795cf695a0
adl.mali@xl.co.id <adl.mali@xl.co.id>
2024-12-17 01:03:44 +0700

    add main.py

---
+++ data-collection-service/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from scheduler.task import schedule_scraping_job
+
+import asyncio
+
+app = FastAPI(title="Data Collection Service")
+
+@app.get("/")
+async def root():
+    return {"message": "Data Collection Service is Running"}
+
+@app.post("/start-scraping")
+async def start_scraping_job():
+    # Trigger scraping job
+    asyncio.create_task(schedule_scraping_job())
+    return {"status": "Scraping job started!"}
